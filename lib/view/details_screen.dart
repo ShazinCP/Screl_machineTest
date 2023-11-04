add5fdf7c969e1d3ffc167853d9d78d2f978d2ed
Shazin Muhammed <122765461+ShazinCP@users.noreply.github.com>
2023-11-04 12:50:40 +0530

    second commit

--- lib/view/details_screen.dart
+++ lib/view/details_screen.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatelessWidget {
-  const DetailsScreen({super.key});
+  double name;
+  double street;
+  double email;
+  double suit;
+  double city;
+  double zipcode;
+  double lat;
+  double lng;
+
+   DetailsScreen({super.key,required this.city,required this.email,required this.lat,required this.lng,required this.name,required this.street,required this.suit,required this.zipcode});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold());
+    return SafeArea(child: Scaffold(
+      body: Column(
+        // children: [
+        //   Text(),
+        //   Text(),
+        //   Text(),
+        //   Text(),
+        //   Text(),
+        //   Text(),
+        //   Text(),
+        //   Text(),
+        // ],
+      ),
+    ));
   }
 }
--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:screl_machine_test/controller/user_provider.dart';
+import 'package:screl_machine_test/view/details_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -19,11 +20,25 @@ class HomeScreen extends StatelessWidget {
             builder: (context, value, child) {
               return ListView.separated(
                 itemBuilder: (context, index) {
-                return Card(
-                  child: ListTile(
-                    title: Text("${value.allLIst[index].name}"),
-                    subtitle: Text("${value.allLIst[index].street}")
-                    ),
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(builder: DetailsScreen(
+                      city: value.allLIst[index].city,
+                    email: value.allLIst[index].city,
+                    lat: value.allLIst[index].city,
+                    lng: value.allLIst[index].city,
+                    name: value.allLIst[index].city,
+                    street: value.allLIst[index].city,
+                    suit: value.allLIst[index].city,
+                    zipcode: value.allLIst[index].city,
+                    )));
+                  },
+                  child: Card(
+                    child: ListTile(
+                      title: Text("${value.allLIst[index].name}"),
+                      subtitle: Text("${value.allLIst[index].street}")
+                      ),
+                  ),
                 );
               }, separatorBuilder: (context, index) {
                return const Divider();
